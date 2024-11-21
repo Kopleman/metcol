5fdf7511f7488a18b192d7a928695080d218bf20
Алексей Двуреченский <Aleksey.Dvurechenskiy@leroymerlin.ru>
2024-11-21 18:49:10 +0300

    feat(iter1) add updated controller

---
+++ internal/controllers/update_metrics.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"errors"
+	"github.com/Kopleman/metcol/internal/metrics"
+	"net/http"
+)
+
+func UpdateController(metricsService metrics.IMetrics) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		metricTypeStringAsString := req.PathValue("metricType")
+		metricType, err := metrics.ParseMetricType(metricTypeStringAsString)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		metricName := req.PathValue("metricName")
+		if len(metricName) == 0 {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+
+		metricValue := req.PathValue("metricValue")
+		if len(metricValue) == 0 {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		err = metricsService.SetMetric(metricType, metricName, metricValue)
+
+		if errors.Is(err, metrics.ErrValueParse) {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if err := metricsService.SetMetric(metricType, metricName, metricValue); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("content-type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+	}
+}
