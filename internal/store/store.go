fdbca8cef28f54cdb273778f92c739d7e8d3dfc0
Алексей Двуреченский <Aleksey.Dvurechenskiy@leroymerlin.ru>
2024-11-21 18:48:34 +0300

    feat(iter1) add store service

---
+++ internal/store/store.go
@@ -0,0 +1,63 @@
+package store
+
+type IStore interface {
+	Create(key string, value any) error
+	Read(key string) (any, error)
+	Update(key string, value any) error
+	Delete(key string) error
+}
+
+func (s *Store) existed(key string) bool {
+	_, existed := s.db[key]
+	return existed
+}
+
+func (s *Store) Create(key string, value any) error {
+	if s.existed(key) {
+		return ErrAlreadyExists
+	}
+
+	s.db[key] = value
+
+	return nil
+}
+
+func (s *Store) Read(key string) (any, error) {
+	value, existed := s.db[key]
+
+	if !existed {
+		return nil, ErrNotFound
+	}
+
+	return value, nil
+}
+
+func (s *Store) Update(key string, value any) error {
+	if _, err := s.Read(key); err != nil {
+		return err
+	}
+
+	s.db[key] = value
+
+	return nil
+}
+
+func (s *Store) Delete(key string) error {
+	if _, err := s.Read(key); err != nil {
+		return err
+	}
+
+	delete(s.db, key)
+
+	return nil
+}
+
+type Store struct {
+	db map[string]any
+}
+
+func NewStore(db map[string]any) IStore {
+	return &Store{
+		db,
+	}
+}
