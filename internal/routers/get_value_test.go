426272cd1e89dd6eb5606b8eb1f6e6254ef05d15
Алексей Двуреченский <sinmanriv@gmail.com>
2024-11-26 12:44:05 +0300

    fix(iter3) fix unhandled err

--- internal/routers/get_value_test.go
+++ internal/routers/get_value_test.go
@@ -2,6 +2,7 @@
 package routers
 
 import (
+	"errors"
 	"github.com/Kopleman/metcol/internal/metrics"
 	"github.com/Kopleman/metcol/internal/store"
 	"github.com/stretchr/testify/assert"
@@ -20,7 +21,10 @@ func testRequest(t *testing.T, ts *httptest.Server, method,
 
 	resp, err := ts.Client().Do(req)
 	require.NoError(t, err)
-	defer resp.Body.Close()
+	defer func() {
+		err = errors.Join(err, resp.Body.Close())
+		require.NoError(t, err)
+	}()
 
 	respBody, err := io.ReadAll(resp.Body)
 	require.NoError(t, err)
