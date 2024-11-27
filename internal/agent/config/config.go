dbc066be6af23ce746206de4f003e7b134d846f5
Алексей Двуреченский <sinmanriv@gmail.com>
2024-11-27 17:07:15 +0300

    feat(iter5) add config enrichment from env

--- internal/agent/config/config.go
+++ internal/agent/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	"github.com/Kopleman/metcol/internal/common/flags"
+	"github.com/caarlos0/env/v6"
 )
 
 type Config struct {
@@ -11,7 +12,14 @@ type Config struct {
 	PollInterval   int64
 }
 
+type configFromEnv struct {
+	EndPoint       string `env:"ADDRESS"`
+	ReportInterval int64  `env:"REPORT_INTERVAL"`
+	PollInterval   int64  `env:"POLL_INTERVAL"`
+}
+
 func ParseAgentConfig() *Config {
+	cfgFromEnv := new(configFromEnv)
 	config := new(Config)
 	netAddr := new(flags.NetAddress)
 	netAddr.Host = "localhost"
@@ -26,6 +34,24 @@ func ParseAgentConfig() *Config {
 	flag.Int64Var(&config.PollInterval, "p", 2, "poll interval")
 
 	flag.Parse()
+
+	if err := env.Parse(cfgFromEnv); err != nil {
+		panic(err)
+	}
+
+	if cfgFromEnv.EndPoint != "" {
+		if err := netAddr.Set(cfgFromEnv.EndPoint); err != nil {
+			panic(err)
+		}
+	}
+
+	if cfgFromEnv.PollInterval > 0 {
+		config.PollInterval = cfgFromEnv.PollInterval
+	}
+
+	if cfgFromEnv.ReportInterval > 0 {
+		config.ReportInterval = cfgFromEnv.ReportInterval
+	}
 
 	return config
 }
--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -3,13 +3,19 @@ package config
 import (
 	"flag"
 	"github.com/Kopleman/metcol/internal/common/flags"
+	"github.com/caarlos0/env/v6"
 )
 
 type Config struct {
 	NetAddr *flags.NetAddress
 }
 
+type configFromEnv struct {
+	EndPoint string `env:"ADDRESS"`
+}
+
 func ParseServerConfig() *Config {
+	cfgFromEnv := new(configFromEnv)
 	config := new(Config)
 	netAddr := new(flags.NetAddress)
 	netAddr.Host = "localhost"
@@ -19,6 +25,16 @@ func ParseServerConfig() *Config {
 	netAddrValue := flag.Value(netAddr)
 	flag.Var(netAddrValue, "a", "address and port to run server")
 	flag.Parse()
+
+	if err := env.Parse(cfgFromEnv); err != nil {
+		panic(err)
+	}
+
+	if cfgFromEnv.EndPoint != "" {
+		if err := netAddr.Set(cfgFromEnv.EndPoint); err != nil {
+			panic(err)
+		}
+	}
 
 	return config
 }
