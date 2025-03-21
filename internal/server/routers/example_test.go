0ddb651b8d5ae645858e606788dd4813eaae82b5
Алексей Двуреченский <sinmanriv@gmail.com>
2025-03-21 13:54:12 +0300

    fix(iter18) examples

--- internal/server/routers/example_test.go
+++ internal/server/routers/example_test.go
@@ -20,7 +20,7 @@ import (
 
 type noopPgxPool struct{}
 
-func (p *noopPgxPool) Ping(_ context.Context) error {
+func (p *noopPgxPool) Ping(err context.Context) error {
 	return nil
 }
 
@@ -32,44 +32,59 @@ func setupRouter(baseMemStore map[string]*dto.MetricDTO) *chi.Mux {
 	return routes
 }
 
-func ExampleBuildServerRoutes_updateMetric() {
+func ExampleBuildServerRouteserrupdateMetric() {
 	router := setupRouter(make(map[string]*dto.MetricDTO))
 
 	ts := httptest.NewServer(router)
 	defer ts.Close()
 
 	// Update metric via URL params
-	paramReq, _ := http.NewRequest("POST", ts.URL+"/update/gauge/test_metric/123.45", nil)
-	paramsResp, _ := http.DefaultClient.Do(paramReq)
+	paramReq, err := http.NewRequest("POST", ts.URL+"/update/gauge/testerrmetric/123.45", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	paramsResp, err := http.DefaultClient.Do(paramReq)
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer paramsResp.Body.Close()
 
 	// Update metric via JSON
 	jsonBody := []byte(`{
-		"id": "test_counter",
+		"id": "testerrcounter",
 		"type": "counter",
 		"delta": 10
 	}`)
-	jsonReq, _ := http.NewRequest("POST", ts.URL+"/update", bytes.NewBuffer(jsonBody))
+	jsonReq, err := http.NewRequest("POST", ts.URL+"/update", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		fmt.Println(err)
+	}
 	jsonReq.Header.Set("Content-Type", "application/json")
-	jsonResp, _ := http.DefaultClient.Do(jsonReq)
-	body, _ := io.ReadAll(jsonResp.Body)
+	jsonResp, err := http.DefaultClient.Do(jsonReq)
+	if err != nil {
+		fmt.Println(err)
+	}
+	body, err := io.ReadAll(jsonResp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer jsonResp.Body.Close()
 	fmt.Println(string(body))
 
 	// Output:
-	// {"delta":10,"id":"test_counter","type":"counter"}
+	// {"delta":10,"id":"testerrcounter","type":"counter"}
 }
 
-func ExampleBuildServerRoutes_getMetric() {
+func ExampleBuildServerRouteserrgetMetric() {
 	mockStore := map[string]*dto.MetricDTO{
-		"test_metric-gauge": {
-			ID:    "test_metric",
+		"testerrmetric-gauge": {
+			ID:    "testerrmetric",
 			MType: "gauge",
 			Delta: nil,
 			Value: testutils.Pointer(1.1),
 		},
-		"test_metric-counter": {
-			ID:    "test_metric",
+		"testerrmetric-counter": {
+			ID:    "testerrmetric",
 			MType: "counter",
 			Delta: testutils.Pointer(int64(100)),
 			Value: nil,
@@ -80,51 +95,78 @@ func ExampleBuildServerRoutes_getMetric() {
 	defer ts.Close()
 
 	// Fetch metric via URL params
-	paramReq, _ := http.NewRequest("GET", ts.URL+"/value/counter/test_metric", nil)
-	paramResp, _ := http.DefaultClient.Do(paramReq)
-	body, _ := io.ReadAll(paramResp.Body)
+	paramReq, err := http.NewRequest("GET", ts.URL+"/value/counter/testerrmetric", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	paramResp, err := http.DefaultClient.Do(paramReq)
+	if err != nil {
+		fmt.Println(err)
+	}
+	body, err := io.ReadAll(paramResp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer paramResp.Body.Close()
 	fmt.Println(string(body))
 
 	// Fetch metric via JSON
 	jsonBody := []byte(`{
-		"id": "test_metric",
+		"id": "testerrmetric",
 		"type": "gauge"
 	}`)
-	jsonReq, _ := http.NewRequest("POST", ts.URL+"/value", bytes.NewBuffer(jsonBody))
+	jsonReq, err := http.NewRequest("POST", ts.URL+"/value", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		fmt.Println(err)
+	}
 	jsonReq.Header.Set("Content-Type", "application/json")
-	jsonResp, _ := http.DefaultClient.Do(jsonReq)
-	body, _ = io.ReadAll(jsonResp.Body)
+	jsonResp, err := http.DefaultClient.Do(jsonReq)
+	if err != nil {
+		fmt.Println(err)
+	}
+	body, err = io.ReadAll(jsonResp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer jsonResp.Body.Close()
 	fmt.Println(string(body))
 
 	// Output:
 	// 100
-	// {"value":1.1,"id":"test_metric","type":"gauge"}
+	// {"value":1.1,"id":"testerrmetric","type":"gauge"}
 }
 
-func ExampleBuildServerRoutes_batchUpdate() {
+func ExampleBuildServerRouteserrbatchUpdate() {
 	router := setupRouter(make(map[string]*dto.MetricDTO))
 	ts := httptest.NewServer(router)
 	defer ts.Close()
 
 	jsonBody := []byte(`[{
-		"id": "test_counter",
+		"id": "testerrcounter",
 		"type": "counter",
 		"delta": 5
 	}, {
-		"id": "test_gauge",
+		"id": "testerrgauge",
 		"type": "gauge",
 		"value": 3.14
 	}]`)
 
-	req, _ := http.NewRequest("POST", ts.URL+"/updates", bytes.NewBuffer(jsonBody))
+	req, err := http.NewRequest("POST", ts.URL+"/updates", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		fmt.Println(err)
+	}
 	req.Header.Set("Content-Type", "application/json")
-	resp, _ := http.DefaultClient.Do(req)
-	body, _ := io.ReadAll(resp.Body)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer resp.Body.Close()
 	fmt.Println(string(body))
 
 	// Output:
-	// [{"delta":5,"id":"test_counter","type":"counter"},{"value":3.14,"id":"test_gauge","type":"gauge"}]
+	// [{"delta":5,"id":"testerrcounter","type":"counter"},{"value":3.14,"id":"testerrgauge","type":"gauge"}]
 }
