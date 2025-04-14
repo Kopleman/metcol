37e12f6de741e243a89be914dff5845662648bbd
Алексей Двуреченский <sinmanriv@gmail.com>
2025-04-14 11:27:07 +0300

    feat(iter21) add crypto key generator

---
+++ internal/utils/crypto_keys_generator/crypto_keys_generator.go
@@ -0,0 +1,102 @@
+// Package cryptokeysgenerator used to create keys pair
+package cryptokeysgenerator
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"os"
+)
+
+// Generator instance.
+type Generator struct {
+}
+
+func NewGenerator() *Generator {
+	return &Generator{}
+}
+
+func (g *Generator) GenerateKeys(privateKeyPath, publicKeyPath string) error {
+	if publicKeyPath == "" || privateKeyPath == "" {
+		return fmt.Errorf("publicKeyPath or privateKeyPath is empty")
+	}
+
+	if publicKeyPath == privateKeyPath {
+		return fmt.Errorf("publicKeyPath is equal to privateKeyPath")
+	}
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return fmt.Errorf("failed to generate private key pair: %s", err)
+	}
+
+	if pubExportErr := g.exportPublicKeyToFile(&privateKey.PublicKey, publicKeyPath); pubExportErr != nil {
+		return fmt.Errorf("failed to export public key to file: %s", pubExportErr)
+	}
+
+	if privateExportErr := g.exportPrivateKeyToFile(privateKey, privateKeyPath); privateExportErr != nil {
+		return fmt.Errorf("failed to export private key to file: %s", privateExportErr)
+	}
+
+	return nil
+}
+
+func (g *Generator) exportPublicKeyToFile(publicKey *rsa.PublicKey, pathToFile string) error {
+	pubKeyBytes, err := x509.MarshalPKIXPublicKey(publicKey)
+	if err != nil {
+		return fmt.Errorf("failed to serialize public key: %s", err)
+	}
+	pubKeyPEM := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "PUBLIC KEY",
+			Bytes: pubKeyBytes,
+		},
+	)
+
+	file, fileErr := g.getFileDescriptor(pathToFile)
+	if fileErr != nil {
+		return fmt.Errorf("failed to get file descriptor for pub-key: %s", fileErr)
+	}
+
+	if _, writeErr := file.WriteString(string(pubKeyPEM)); writeErr != nil {
+		return fmt.Errorf("failed to write public key to file: %s", writeErr)
+	}
+
+	return nil
+}
+
+func (g *Generator) exportPrivateKeyToFile(privateKey *rsa.PrivateKey, pathToFile string) error {
+	privKeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
+	privKeyPEM := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: privKeyBytes,
+		},
+	)
+
+	file, fileErr := g.getFileDescriptor(pathToFile)
+	if fileErr != nil {
+		return fmt.Errorf("failed to get file descriptor for private-key: %s", fileErr)
+	}
+
+	if _, writeErr := file.WriteString(string(privKeyPEM)); writeErr != nil {
+		return fmt.Errorf("failed to write private key to file: %s", writeErr)
+	}
+
+	return nil
+}
+
+func (g *Generator) getFileDescriptor(path string) (*os.File, error) {
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0o666) //nolint:all // different lint behavior on perm var
+	if err != nil {
+		return nil, fmt.Errorf("could not access file by path '%s': %w", path, err)
+	}
+
+	if err = file.Truncate(0); err != nil {
+		return nil, fmt.Errorf("could not file: %w", err)
+	}
+
+	return file, nil
+}
