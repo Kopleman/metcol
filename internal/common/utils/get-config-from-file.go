fe9db541f1c133ea1e48a3b2e8d3206552b2efcd
Алексей Двуреченский <sinmanriv@gmail.com>
2025-04-17 17:34:35 +0300

    fix(iter22) fix defer

--- internal/common/utils/get-config-from-file.go
+++ internal/common/utils/get-config-from-file.go
@@ -8,10 +8,10 @@ import (
 
 func GetConfigFromFile(path string, dest any) error {
 	file, err := os.Open(path)
-	defer file.Close() ///nolint:all // close
 	if err != nil {
 		return fmt.Errorf("could not open config file: %w", err)
 	}
+	defer file.Close() ///nolint:all // close
 
 	decoder := json.NewDecoder(file)
 	if decodeErr := decoder.Decode(dest); decodeErr != nil {
